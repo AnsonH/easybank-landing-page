f1c2def8d269004ced15e67d4935b3ac35e656f1
Anson Heung <ansonheung2000@gmail.com>
2021-02-26 22:55:06 +0800

    onclick for mobile menu overlay

--- app/js/script.js
+++ app/js/script.js
@@ -2,16 +2,21 @@ const body = document.querySelector("body");
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const fadeElements = document.querySelectorAll(".has-fade");
+const headerMobileMenu = document.querySelector(".header__menu");
+
+// Close the hamburger menu
+function closeMobileMenu() {
+  body.classList.remove("noscroll");
+  header.classList.remove("open");
+  fadeElements.forEach((item) => {
+    item.classList.remove("fade-in");
+    item.classList.add("fade-out");
+  });
+}
 
 btnHamburger.addEventListener("click", function () {
   if (header.classList.contains("open")) {
-    // Close hamburger menu
-    body.classList.remove("noscroll");
-    header.classList.remove("open");
-    fadeElements.forEach((item) => {
-      item.classList.remove("fade-in");
-      item.classList.add("fade-out");
-    });
+    closeMobileMenu();
   } else {
     // Open hamburger menu
     body.classList.add("noscroll");
@@ -20,5 +25,13 @@ btnHamburger.addEventListener("click", function () {
       item.classList.add("fade-in");
       item.classList.remove("fade-out");
     });
+  }
+});
+
+// Close the mobile menu when click on the overlay
+const overlay = document.querySelector(".overlay");
+overlay.addEventListener("click", function () {
+  if (header.classList.contains("open")) {
+    closeMobileMenu();
   }
 });
